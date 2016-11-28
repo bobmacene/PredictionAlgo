d519523c0f29a0f0b1ae1c2866a82e4d3ec5304d
bobmack <bobbymaceneaney@gmail.com>
2016-11-28 08:39:14 +0000

    url update

--- PredictionAlgo/Models/DataModel/BettingData.cs
+++ PredictionAlgo/Models/DataModel/BettingData.cs
@@ -9,9 +9,9 @@ namespace PredictionAlgo.Models.DataModel
 {
     public class BettingData
     {
-        //private const string HCapCouponPath = @"http://www.paddypower.com/bet/rugby-union/rugby-hcap-coupon";
-        private const string HCapCouponPath = "file://C:/Users/bob/Documents/" + "ITT Project/BettingHtml/2016.10.07_Rugby H'Cap Betting from Paddy Power.html";
-        private readonly string _uriHCapCoupon = new Uri(HCapCouponPath).LocalPath;
+        private const string HCapCouponPath = @"http://www.paddypower.com/bet/rugby-union/rugby-hcap-coupon";
+        //private const string HCapCouponPath = "file://C:/Users/bob/Documents/" + "ITT Project/BettingHtml/2016.10.07_Rugby H'Cap Betting from Paddy Power.html";
+        private readonly string _uriHCapCoupon = new Uri(HCapCouponPath).AbsolutePath;
         public ICollection<MatchBettingData> MatchBettingDataList { get; set; }
         private readonly PredictionAlgoContext _db = new PredictionAlgoContext();
         public ICollection<MatchBettingData> GetCurrentBettingData
