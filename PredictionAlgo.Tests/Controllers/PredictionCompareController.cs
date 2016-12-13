7ce8b0f8695ae669b00a046bb919882532246338
bobmack <bobbymaceneaney@gmail.com>
2016-12-13 15:03:14 +0000

    c

---
+++ PredictionAlgo.Tests/Controllers/PredictionCompareController.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Web.Mvc;
+using NUnit.Framework;
+using PredictionAlgo.Controllers;
+
+namespace PredictionAlgo.Tests.Controllers
+{
+    [TestFixture]
+    public class PredictionCompareController
+    {
+        [Test]
+        public void PredictionComparisonsController_IndexNotNull_True()
+        {
+            using (var controller = new PredictionComparisonsController())
+            {
+                var result = controller.Index(string.Empty) as ViewResult;
+
+                Assert.IsNotNull(result);
+            }
+        }
+
+
+    }
+}
